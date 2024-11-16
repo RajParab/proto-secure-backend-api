8f1b485230a3b8e671da91056767806df43fbc8a
RajParab <raj.parab99@yahoo.com>
2024-11-16 17:41:28 +0700

    add route for update status

--- src/routes/dapp.route.js
+++ src/routes/dapp.route.js
@@ -6,6 +6,7 @@ if (process.env.SERVER == "testnet") {
     router.post('/submit-form', dappController.addProjectForm);
     router.post('/update-contract', dappController.updateDappForm);
     router.get('/list-projects', dappController.listDapps);
+    router.get('/update-status', dappController.changeStatus);
 }
 
 module.exports = router;
