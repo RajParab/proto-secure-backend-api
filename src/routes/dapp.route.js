29927dfcb270cbe11d492cd9aca04b20d2860eae
RajParab <raj.parab99@yahoo.com>
2024-11-16 20:54:02 +0700

    fix route

--- src/routes/dapp.route.js
+++ src/routes/dapp.route.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const dappController = require("../controllers/dapp.controller")
+const dappController = require('../controllers/dapp.controller');
 const router = express.Router();
 
-if (process.env.SERVER == "testnet") {
-    router.post('/submit-form', dappController.addProjectForm);
-    router.post('/update-contract/:chainID', dappController.updateDappForm);
-    router.get('/list-projects', dappController.listDapps);
-    router.post('/update-status/:chainID', dappController.changeStatus);
-}
+router.post('/submit-form', dappController.addProjectForm);
+router.post('/update-contract/:chainID', dappController.updateDappForm);
+router.get('/list-projects', dappController.listDapps);
+router.post('/update-status/:chainID', dappController.changeStatus);
 
 module.exports = router;
